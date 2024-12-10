64c5e9aaf800aa2d94e56a97eb8aa72b2d432154
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-12-10 18:34:58 +0500

    group filter

--- filters/private_chat.py
+++ filters/private_chat.py
@@ -1,13 +1,11 @@
-from typing import Union
-
 from aiogram.enums import ChatType
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 
 class ChatPrivateFilter(BaseFilter):
-    def __init__(self, chat_type: Union[str, list]):
-        self.chat_type = chat_type
+    def __init__(self, is_private: bool = True):
+        self.is_private = is_private
 
     async def __call__(self, message: Message) -> bool:
-        return message.chat.type == ChatType.PRIVATE
+        return (message.chat.type == ChatType.PRIVATE) == self.is_private
