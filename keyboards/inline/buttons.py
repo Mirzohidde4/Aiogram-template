cbff7bbaf697d6bbc31473c87fd2bacfcbb7b0b2
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2025-01-19 14:02:06 +0500

    buttons

--- keyboards/inline/buttons.py
+++ keyboards/inline/buttons.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def CreateInline(*button_rows, just=int) -> InlineKeyboardMarkup: #! {a: a, b: b}
+def CreateInline(*button_rows, just=(1,)) -> InlineKeyboardMarkup: #! {a: a, b: b}
     builder = InlineKeyboardBuilder()
     for row in button_rows:
         for text, callback_data in row.items():
@@ -10,5 +10,5 @@ def CreateInline(*button_rows, just=int) -> InlineKeyboardMarkup: #! {a: a, b: b
                 builder.add(InlineKeyboardButton(text=text,url=callback_data))
             else:
                 builder.add(InlineKeyboardButton(text=text, callback_data=callback_data))
-    builder.adjust(just)
+    builder.adjust(*just)
     return builder.as_markup()
--- keyboards/reply/buttons.py
+++ keyboards/reply/buttons.py
@@ -2,9 +2,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
-def Createreply(*args, contact=False, just=int) -> ReplyKeyboardBuilder: #! 'a', 'b', ..
+def Createreply(*args, contact=False, just=(2,)) -> ReplyKeyboardBuilder: #! 'a', 'b', ..
     bulder = ReplyKeyboardBuilder()
     for i in args:
         bulder.add(KeyboardButton(text=i, request_contact=True if contact else False))
-    bulder.adjust(just)
+    bulder.adjust(*just)
     return bulder.as_markup(resize_keyboard=True, one_time_keyboard=True)
